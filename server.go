3065296597c61b5e54e599304267785f2bd44c3d
Will Kennedy <will.kennedy@wisetechglobal.com>
2023-06-07 20:15:51 +1000

    Added CORSMiddleware

--- server.go
+++ server.go
@@ -26,19 +26,35 @@ func main() {
 		gin.SetMode(gin.ReleaseMode)
 	}
 
-	router := gin.Default()
+	router := gin.New()
+	router.Use(CORSMiddleware())
 
 	router.GET("/products", getProducts)
-	router.Any("/payment", createPaymentIntent)
+	router.POST("/payment", createPaymentIntent)
 
 	port := os.Getenv("PORT")
 	router.Run(":" + port)
 }
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func getProducts(c *gin.Context) {
 	items := stripeGetProducts()
-
-	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 
 	// if dev, use indented json
 	if gin.Mode() == gin.DebugMode {
@@ -49,28 +65,22 @@ func getProducts(c *gin.Context) {
 }
 
 func createPaymentIntent(c *gin.Context) {
-	if (c.Request.Method == "POST") {
-		type cart struct {
-			Items []cart_item `json:"items"`
-		}
+	type cart struct {
+		Items []cart_item `json:"items"`
+	}
 
-		var body cart
-		c.BindJSON(&body)
-		// TODO - make sure cart items have quantities and IDs
+	var body cart
+	c.BindJSON(&body)
+	// TODO - make sure cart items have quantities and IDs
 
-		i := stripeCreatePaymentIntent(body.Items)
+	i := stripeCreatePaymentIntent(body.Items)
 
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 
-		// if dev, use indented json
-		if gin.Mode() == gin.DebugMode {
-			c.IndentedJSON(http.StatusOK, i)
-		} else {
-			c.JSON(http.StatusOK, i)
-		}
+	// if dev, use indented json
+	if gin.Mode() == gin.DebugMode {
+		c.IndentedJSON(http.StatusOK, i)
 	} else {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE")
-		c.Status(http.StatusNoContent)
+		c.JSON(http.StatusOK, i)
 	}
 }
